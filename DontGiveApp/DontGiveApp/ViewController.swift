cff8871136f2665bbc5282d769bb7e97e99faf29
Guilherme Dall Agnol <dallguilherme@gmail.com>
2019-09-19 10:16:00 -0300

    Teste

--- DontGiveApp/DontGiveApp/ViewController.swift
+++ DontGiveApp/DontGiveApp/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //teste
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.red
     }
 
 
